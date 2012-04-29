22ab100814d8547057b50fca2f2b8fed3f6065c9
Arunoda <arunoda.susiripala@gmail.com>
2012-04-29 17:56:49 +0530

    checked siblings and added nth-child

--- jquery.signature.js
+++ jquery.signature.js
@@ -9,25 +9,26 @@
 
 		for(var llc=0; llc<els.length; llc++) {
 
-			var e = els[llc];
+			var e = $(els[llc]);
 
-			var id = $(e).attr('id');
-			var clas = $(e).attr('class');
-			var type = $(e).get(0).tagName;
+			var id = e.attr('id');
+			var clas = e.attr('class');
+			var type = e.get(0).tagName;
 
-			var index = $(e).index();
+			var index = e.index();
+
+			var combinator = " ";
+			if(e.siblings().length > 1) {
+				combinator = ":nth-child(" + (index + 1) + ") "
+			}
 
 			if(id) {
 				signature = "#" + id + " " + signature;
 			} else if(clas) {
-				signature = '.' + clas + " " + signature;
+				signature = '.' + clas + combinator + signature;
 			} else if(type != 'HTML') {
 	
-				if(llc==0) {
-					signature = type.toLowerCase() + ":nth-child(" + (index + 1) + ") " + signature;
-				} else {
-					signature = type.toLowerCase() + " " + signature;
-				}
+				signature = type.toLowerCase() + combinator + signature;
 			}
 		}
 
